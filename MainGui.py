6342f6224a3fe366329e8f2d1a5a1d2ceaf9d64d
vduyhai <duyhai1999.work@gmail.com>
2022-04-04 13:58:56 +0700

    Add 3D files

--- MainGui.py
+++ MainGui.py
@@ -1,6 +1,6 @@
 from PyQt5 import QtGui, QtWidgets, QtCore
 import sys
-import welcomescreen, pick, squat1
+import welcomescreen, pick, squat1, camera
 from time import sleep
 import cv2 as cv
 # <name>.setWindowFlags(QtCore.Qt.FramelessWindowHint)
@@ -23,27 +23,15 @@ def pickUI():
     ui = pick.Ui_MainWindow()
     ui.setupUi(MainWindow)
     ui.cancel.clicked.connect(startUI)
-    ui.coffee.clicked.connect(squat1UI)
+    # ui.coffee.clicked.connect(cameraUI)
     MainWindow.show()
 
-def squat1UI():
-    global ui
-    ui = squat1.Ui_Dialog()
-    ui.setupUi(MainWindow)
-    cap = cv.VideoCapture(0)
-    while True:
-        _, frame = cap.read()
-        frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+# def cameraUI():
+#     global ui
+#     ui = camera.Ui_Dialog()
+#     ui.setupUi(MainWindow)
+#     MainWindow.show()
 
-        h,w, c = frame.shape
-        step = c * w
-
-        qFrame = QtGui.QImage(frame.data, w, h, step, QtGui.QImage.Format_RGB888)
-
-        ui.label.setPiximap(QtGui.QPixmap.fromImage(qFrame))
-
-        MainWindow.show()
-        cv.waitKey(1)
 
 startUI()
 sys.exit(app.exec_())
--- camera.py
+++ camera.py
@@ -15,15 +15,8 @@ class MainWindow(QMainWindow):
         self.uic = Ui_Dialog()
         self.uic.setupUi(self)
 
-        # self.uic.Button_start.clicked.connect(self.start_capture_video)
-        # self.uic.Button_stop.clicked.connect(self.stop_capture_video)
-
         self.thread = {}
-    # def closeEvent(self, event):
-    #     self.stop_capture_video()
-    # def stop_capture_video(self):
-    #     self.thread[1].stop()
-    # def start_capture_video(self):
+
         self.thread[1] = capture_video(index=1)
         self.thread[1].start()
         self.thread[1].signal.connect(self.show_wedcam)
@@ -39,7 +32,7 @@ class MainWindow(QMainWindow):
         h, w, ch = rgb_image.shape
         bytes_per_line = ch * w
         convert_to_Qt_format = QtGui.QImage(rgb_image.data, w, h, bytes_per_line, QtGui.QImage.Format_RGB888)
-        p = convert_to_Qt_format.scaled(400, 600, Qt.KeepAspectRatio)
+        p = convert_to_Qt_format.scaled(480, 640, Qt.KeepAspectRatio)
         return QPixmap.fromImage(p)
 
 class capture_video(QThread):
@@ -50,7 +43,7 @@ class capture_video(QThread):
         super(capture_video, self).__init__()
 
     def run(self):
-        cap = cv2.VideoCapture(0)  # 'D:/8.Record video/My Video.mp4'
+        cap = cv2.VideoCapture(0)
         while True:
             ret, cv_img = cap.read()
             if ret:
